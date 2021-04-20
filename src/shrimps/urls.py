68d96d94811126f6e08e266fdfc15ca519fbd558
Eva <ubybc@student.kit.edu>
2021-04-20 21:22:30 +0100

    added trailing slash to make sure the url path give-birth is understood

--- src/shrimps/urls.py
+++ src/shrimps/urls.py
@@ -4,5 +4,5 @@ from shrimps.views import home, give_birth_to_shrimp
 
 urlpatterns = [
     path('', home, name="home"),  # default path (null) so doesn't need any path string
-    path('give-birth', give_birth_to_shrimp, name="give-birth"),
+    path('give-birth/', give_birth_to_shrimp, name="give-birth"),
 ]
